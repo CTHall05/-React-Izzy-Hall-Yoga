ca04c6707519972232551e913331c8ff091fcf0f
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-05-06 19:22:41 +1000

    Updated fetch request in the server.js file.

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,70 @@
-const nodemailer = require('nodemailer');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
-});
+app.use(cors()); // enable CORS for all routes
 
 app.use(express.json());
 
 app.post('/api/bookings', (req, res) => {
-  // extract form data from the request body
-  const { name, email, dateTime } = req.body;
+  const { name, email, classTime, selectedDate } = req.body;
 
-  // do something with the form data (e.g. save to a database)
+  // Do something with the form data (e.g., save to a database)
 
-  // send confirmation email to the user
-  const userMailOptions = {
-    from: 'your-email@gmail.com',
-    to: email,
-    subject: 'Booking confirmation',
-    text: `Dear ${name},\n\nThank you for booking on ${dateTime}. We look forward to seeing you soon.\n\nBest regards,\nYour Yoga Studio`
-  };
+  // Assuming you're using Nodemailer to send the confirmation email
+  const nodemailer = require('nodemailer');
 
-  // send notification email to the business owner
-  const ownerMailOptions = {
-    from: 'your-email@gmail.com',
-    to: 'recipient-email@gmail.com',
-    subject: 'New booking',
-    text: `${name} has booked on ${dateTime}.`
-  };
-
-  // send confirmation email to the user and notification email to the business owner
+// Create a transporter using the SMTP configuration
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-password'
+      user: 'charlestaylorhall05@gmail.com', // Replace with your Gmail address
+      pass: 'zdcqktcgxjrecjqe' // Replace with your Gmail password or app password if 2-Step Verification is enabled
     }
   });
+
+  // // Use the transporter to send an email
+  // const mailOptions = {
+  //   from: 'charlestaylorhall05@gmail.com', // Replace with your Gmail address
+  //   to: 'charlestaylorhall05@gmail.com',
+  //   subject: 'Hello',
+  //   text: 'This is a test email.'
+  // };
+
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.error('Error sending email:', error);
+  //   } else {
+  //     console.log('Email sent:', info.response);
+  //   }
+  // });
+
+
+
+  const userMailOptions = {
+    from: 'charlestaylorhall05@gmail.com',
+    to: email,
+    subject: 'Booking confirmation',
+    text: `Dear ${name},\n\nThank you for booking at ${classTime} on ${selectedDate}. We look forward to seeing you soon.\n\nBest regards,\nYour Yoga Studio`
+  };
 
   transporter.sendMail(userMailOptions, (error, info) => {
     if (error) {
-      console.log(error);
+      console.error(error);
+      res.status(500).send('Error sending confirmation email');
     } else {
-      console.log(`Confirmation email sent to ${email}: ${info.response}`);
+      console.log('Confirmation email sent:', info.response);
+
+      // Optionally, send notification email to the business owner
+      // const ownerMailOptions = { ... };
+
+      // res.status(200).send('Booking confirmed');
+      res.json({ message: 'Booking confirmed' });
     }
   });
-
-  transporter.sendMail(ownerMailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(`Notification email sent to business owner: ${info.response}`);
-    }
-  });
-
-  // send response to the client
-  res.status(200).send('Booking confirmed');
 });
 
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
